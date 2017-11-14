d47d81b9c1ffa50ba2a2843cabec42379820dff5
1243428 <a1243428@01HW849271.local>
2017-11-14 12:07:59 +0530

    test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 4000;
 
 app.get('/',function(req,res) {
   res.send("Hello World.");
 });
 
-app.listen(4000);
+app.listen(port);
 console.log('hello..88');
